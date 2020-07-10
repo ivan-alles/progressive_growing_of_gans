3f3699ded6a694fff95e32f755a9d33d389f8b5c
Ivan Alles <ivan.alles@gmail.com>
2020-07-10 11:17:01 +0200

    Make generator example working (a manual pkl file adaptation is needed)

--- import_example.py
+++ import_example.py
@@ -3,8 +3,27 @@ import numpy as np
 import tensorflow as tf
 import PIL.Image
 
+#  Needed for tf.compat.v1.placeholder
+tf.compat.v1.disable_eager_execution()
+
+#  Needed for the original code to run. The pre-trained models are pickled with the code.
+#  We do not want to change it, so the functions they need to the tf module.
+tf.get_default_graph = tf.compat.v1.get_default_graph
+tf.AUTO_REUSE = tf.compat.v1.AUTO_REUSE
+tf.placeholder = tf.compat.v1.placeholder
+tf.variable_scope = tf.compat.v1.variable_scope
+tf.get_variable_scope = tf.compat.v1.get_variable_scope
+tf.get_variable = tf.compat.v1.get_variable
+tf.rsqrt = tf.compat.v1.rsqrt
+tf.initializers.random_normal = tf.compat.v1.initializers.random_normal
+tf.global_variables = tf.compat.v1.global_variables
+tf.trainable_variables = tf.compat.v1.trainable_variables
+tf.get_default_session = tf.compat.v1.get_default_session
+tf.assign = tf.compat.v1.assign
+
+
 # Initialize TensorFlow session.
-tf.InteractiveSession()
+tf.compat.v1.InteractiveSession()
 
 # Import official CelebA-HQ networks.
 with open('karras2018iclr-celebahq-1024x1024.pkl', 'rb') as file:
