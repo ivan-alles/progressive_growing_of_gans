0b7af00b6b28f42348487db7233551b625b15e99
Ivan Alles <ivan.alles@gmail.com>
2020-07-17 10:15:37 +0200

    Fix values of some parameters

--- networks2.py
+++ networks2.py
@@ -18,30 +18,28 @@ def lerp(a, b, t):
 #----------------------------------------------------------------------------
 # Get/create weight tensor for a convolutional or fully-connected layer.
 
-def get_weight(name_prefix, shape, gain=np.sqrt(2), use_wscale=False, fan_in=None):
+def get_weight(name_prefix, shape, gain=np.sqrt(2)):
     weight = tf.Variable(VARIABLES.get(name_prefix + '/weight'), name='weight')
-    if use_wscale:
-        if fan_in is None:
-            fan_in = np.prod(shape[:-1])
-        std = gain / np.sqrt(fan_in)  # He init
-        weight = weight * std
+    fan_in = np.prod(shape[:-1])
+    std = gain / np.sqrt(fan_in)  # He init
+    weight = weight * std
     return weight
 
 #----------------------------------------------------------------------------
 # Fully-connected layer.
 
-def dense(x, name_prefix, fmaps, gain=np.sqrt(2), use_wscale=False):
+def dense(x, name_prefix, fmaps, gain=np.sqrt(2)):
     if len(x.shape) > 2:
         x = tf.reshape(x, [-1, np.prod([d for d in x.shape[1:]])])
-    w = get_weight(name_prefix, [x.shape[1], fmaps], gain=gain, use_wscale=use_wscale)
+    w = get_weight(name_prefix, [x.shape[1], fmaps], gain=gain)
     return tf.matmul(x, w)
 
 #----------------------------------------------------------------------------
 # Convolutional layer.
 
-def conv2d(x, name_prefix, fmaps, kernel, gain=np.sqrt(2), use_wscale=False):
+def conv2d(x, name_prefix, fmaps, kernel, gain=np.sqrt(2)):
     assert kernel >= 1 and kernel % 2 == 1
-    w = get_weight(name_prefix, [kernel, kernel, x.shape[1], fmaps], gain=gain, use_wscale=use_wscale)
+    w = get_weight(name_prefix, [kernel, kernel, x.shape[1], fmaps], gain=gain)
     return tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='SAME', data_format='NCHW')
 
 #----------------------------------------------------------------------------
@@ -72,7 +70,6 @@ def G_paper(
     fmap_base           = 8192,         # Overall multiplier for the number of feature maps.
     fmap_decay          = 1.0,          # log2 feature map reduction when doubling the resolution.
     fmap_max            = 512,          # Maximum number of feature maps in any layer.
-    use_wscale          = True,         # Enable equalized learning rate?
     **kwargs                            # Used to check a possibly pickled unsupported in TF2 version values.
 ):
     def check_arg(name, expected_value):
@@ -87,6 +84,7 @@ def G_paper(
     check_arg('latent_size', None)
     check_arg('dtype', 'float32')
     check_arg('normalize_latents', True)
+    check_arg('use_wscale', True)
 
     global VARIABLES
     VARIABLES = variables
@@ -102,28 +100,28 @@ def G_paper(
         name_prefix = f'{2**res}x{2**res}'
         if res == 2:  # 4x4
             x = PixelNormLayer()(x)
-            x = dense(x, name_prefix=name_prefix + '/Dense', fmaps=nf(res-1)*16, gain=np.sqrt(2)/4, use_wscale=use_wscale) # override gain to match the original Theano implementation
+            x = dense(x, name_prefix=name_prefix + '/Dense', fmaps=nf(res-1)*16, gain=np.sqrt(2)/4) # override gain to match the original Theano implementation
             x = tf.reshape(x, [-1, nf(res-1), 4, 4])
             x = PixelNormLayer()(act(apply_bias(x, name_prefix=name_prefix + '/Dense')))
 
             x = PixelNormLayer()(act(
-                apply_bias(conv2d(x, name_prefix=name_prefix + '/Conv', fmaps=nf(res-1), kernel=3, use_wscale=use_wscale),
+                apply_bias(conv2d(x, name_prefix=name_prefix + '/Conv', fmaps=nf(res-1), kernel=3),
                 name_prefix=name_prefix + '/Conv')))
         else: # 8x8 and up
             x = tf.keras.layers.UpSampling2D(data_format='channels_first', name=name_prefix)(x)
             x = PixelNormLayer()(act(apply_bias(
-                conv2d(x, name_prefix=name_prefix + '/Conv0', fmaps=nf(res-1), kernel=3, use_wscale=use_wscale),
+                conv2d(x, name_prefix=name_prefix + '/Conv0', fmaps=nf(res-1), kernel=3),
                 name_prefix=name_prefix + '/Conv0')))
 
             x = PixelNormLayer()(act(apply_bias(
-                conv2d(x, name_prefix=name_prefix + '/Conv1', fmaps=nf(res-1), kernel=3, use_wscale=use_wscale),
+                conv2d(x, name_prefix=name_prefix + '/Conv1', fmaps=nf(res-1), kernel=3),
                 name_prefix=name_prefix + '/Conv1')))
         return x
     def torgb(x, res): # res = 2..resolution_log2
         lod = resolution_log2 - res
         name_prefix = f'ToRGB_lod{lod}'
         return apply_bias(
-            conv2d(x, name_prefix, fmaps=num_channels, kernel=1, gain=1, use_wscale=use_wscale),
+            conv2d(x, name_prefix, fmaps=num_channels, kernel=1, gain=1),
             name_prefix)
 
     # Recursive structure: complex but efficient.
